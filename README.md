f7cdd94d928b7d06f6a127bb5e8263fb3cf4e0e0
Fabien CHERER <fcherer@finaxys.com>
2024-03-18 17:31:57 +0100

    Ajouter des données dans la base

--- README.md
+++ README.md
@@ -5,3 +5,37 @@
 ```SQL
 // Q : What are SOLID principles ?
 ```
+
+## Utilisation pour créer une table de clients
+
+```SQL
+/*
+Create a table for customers named customer with : 
+- id as a primary, auto-incrementing key
+- first name
+- last name 
+- age as integer
+*/
+```
+
+## Ajouter des données dans la base
+
+### Exemple 1 :
+
+```SQL
+/*
+Insert into the database 6 customers with :
+- French first name 
+- English last name
+- Different first and last names
+- age between 18 and 65
+*/
+```
+
+### Exemple 2 :
+
+```SQL
+/*
+Insert into the database 3 customers with : first name and last name that matches the last 3 presidents of the USA and their age when they were elected
+*/
+```
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,25 @@ CREATE TABLE customer (
     age INT
 );
 
+/*
+Insert into the database 6 customers with :
+- French first name 
+- English last name
+- Different first and last names
+- age between 18 and 65
+*/
+INSERT INTO customer (first_name, last_name, age) VALUES ('Jean', 'Doe', 25);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Pierre', 'Smith', 30);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Paul', 'Johnson', 40);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Jacques', 'Williams', 50);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Marie', 'Brown', 60);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Sophie', 'Taylor', 18);
+
+/*
+Insert into the database 3 customers with : first name and last name that matches the last 3 presidents of the USA and their age when they were elected
+*/
+INSERT INTO customer (first_name, last_name, age) VALUES ('Donald', 'Trump', 70);
+INSERT INTO customer (first_name, last_name, age) VALUES ('Barack', 'Obama', 47);
+INSERT INTO customer (first_name, last_name, age) VALUES ('George', 'Bush', 54);
+
+
