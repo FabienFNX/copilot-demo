118d0b8e303cfbfca01dcc95a9543d89844e4ea1
Fabien CHERER <fcherer@finaxys.com>
2024-03-18 17:26:50 +0100

    Utilisation pour créer une table de clients

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,14 @@
-// Q : What are SOLID principles ?
-// A : SOLID is an acronym for the first five object-oriented design (OOD) principles by Robert C. Martin (also known as Uncle Bob).
-//     These principles, when combined together, make it easy for a programmer to develop software that are easy to maintain and extend.
-//     They also make it easy for developers to avoid code smells, easily refactor code, and are also a part of the agile or adaptive software development.
-//     The SOLID acronym was introduced by Michael Feathers.
-//     The SOLID principles are:
-//     1. Single Responsibility Principle
-//     2. Open/Closed Principle
-//     3. Liskov Substitution Principle
-//     4. Interface Segregation Principle
-//     5. Dependency Inversion Principle
-//     These principles are guidelines that can be applied while working on software to remove code smells by causing the programmer to
+/*
+Create a table for customers named customer with : 
+- id as a primary, auto-incrementing key
+- first name
+- last name 
+- age as integer
+*/
+CREATE TABLE customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    age INT
+);
+
